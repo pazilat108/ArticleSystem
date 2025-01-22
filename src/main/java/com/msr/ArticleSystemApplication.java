229b4322042a9cdb5966567c5dae7a802921ebe5
zx12345 <zx@126.com>
2025-01-22 12:19:00 +0900

    搭建项目的架构

---
+++ src/main/java/com/msr/ArticleSystemApplication.java
@@ -0,0 +1,13 @@
+package com.msr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/msr/ArticleSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
